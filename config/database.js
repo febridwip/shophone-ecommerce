f7841dfbc93301dd35ad393019a1b2ad7970fe8e
Febri Dwi Putro <febri.14116020@student.itera.ac.id>
2020-05-12 14:44:55 +0700

    update user signup & signin auth-jwt
    update user signup & signin auth-jwt

--- config/database.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoDBAtlasURI: 'mongodb+srv://shophone-mongodb:shophone_123456@shophone-mongodb-pe5vu.gcp.mongodb.net/test?retryWrites=true&w=majority'
-};
--- server.js
+++ server.js
@@ -1,30 +1,46 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+require("dotenv").config();
 
+//import routes
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+
+//app
 const app = express();
 
-//bodyParser Middleware
-app.use(bodyParser.json());
-
-const items = require('./routes/api/items');
-
-// db Config
-const db = require('./config/database').mongoDBAtlasURI;
 //connect to mongodb
 mongoose
-  .connect(db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() =>
-    console.log("MongoDB database connection established successfully...")
-  )
-  .catch((err) => console.log(err));
+    .connect(process.env.mongoDB_ATLAS_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() =>
+        console.log("MongoDB database connection established successfully...")
+    )
+    .catch((err) => console.log(err));
 
-// Use Routes
-app.use('/api/items', items);
+// middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+
+    // routes middleware
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+
+//routes
+app.get("/", (req, res) => {
+    res.send("hello from node.js");
+});
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
