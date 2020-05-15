0c61cf8b9284854e941197661a1f0499eeeb6afc
Dewi Rahayu <dewirahayu11mei@gmail.com>
2020-05-15 15:20:50 +0700

    "update node.js - sending products with queries + user profile read & update"
    "update node.js - sending products with queries + user profile read &
    update"

--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    create
+    create,
+    productById,
+    read,
+    remove,
+    update,
+    list,
+    listRelated,
+    listCategories,
+    listBySearch,
+    photo
 } = require("../controllers/product");
 const {
     requireSignin,
@@ -13,8 +22,30 @@ const {
     userById
 } = require("../controllers/user");
 
-router.post("/product/create/:userId", requireSignin, isAdmin, isAdmin, create);
+router.get("/product/:productId", read);
+router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
+router.delete(
+    "/product/:productId/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    remove
+);
+router.put(
+    "/product/:productId/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    update
+);
+
+router.get("/products", list);
+router.get("/products/related/:productId", listRelated);
+router.get("/products/categories", listCategories);
+router.post("/products/by/search", listBySearch);
+router.get("/product/photo/:productId", photo);
 
 router.param("userId", userById);
+router.param("productId", productById);
 
 module.exports = router;
