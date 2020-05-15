6dec24c30ac75cd0ed459d7f14ee25bf30afcccb
Febri Dwi Putro <febri.14116020@student.itera.ac.id>
2020-05-15 09:05:39 +0700

    update

--- routes/product.js
+++ routes/product.js
@@ -2,9 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    create,
-    productById,
-    read
+    create
 } = require("../controllers/product");
 const {
     requireSignin,
@@ -15,10 +13,8 @@ const {
     userById
 } = require("../controllers/user");
 
-router.get("/product/:productId", read);
-router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
+router.post("/product/create/:userId", requireSignin, isAdmin, isAdmin, create);
 
 router.param("userId", userById);
-router.param("productId", productById);
 
 module.exports = router;
