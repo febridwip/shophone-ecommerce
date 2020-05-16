71869beed451337be4185fe37aa190114379d0a4
Febri Dwi Putro <febri.14116020@student.itera.ac.id>
2020-05-16 16:48:50 +0700

    update react.js - private and admin route with user dashboard
    update react.js - private and admin route with user dashboard

--- client/src/core/Layout.js
+++ client/src/core/Layout.js
@@ -6,14 +6,14 @@ const Layout = ({
     description = "Description",
     className,
     children
-}) => ( 
+}) => (
     <div>
-        <Menu/>
-        <div className = "jumbotron">
-            <h2> {title} </h2> 
-            <p className = "lead"> {description} </p> 
-        </div> 
-        <div className = {className}> {children} </div> 
+        <Menu />
+        <div className="jumbotron">
+            <h2>{title}</h2>
+            <p className="lead">{description}</p>
+        </div>
+        <div className={className}>{children}</div>
     </div>
 );
 
